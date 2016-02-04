9ee9a467dc58542180d3d3751c450481b5e4aeed
Gaeldrin <vsetepet@fel.cvut.cz>
2016-02-04 15:57:50 +0100

    Stamping method verified and working, reading from file and hashing data works as well, added various safe-catches to prevent ambiguous exceptions

--- src/com/cesnet/pki/tsa/TSAConnector.java
+++ src/com/cesnet/pki/tsa/TSAConnector.java
@@ -6,24 +6,31 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 import org.bouncycastle.asn1.*;
 import org.bouncycastle.asn1.tsp.TimeStampResp;
 import org.bouncycastle.crypto.digests.SHA1Digest;
+import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.tsp.*;
 
 /**
  * TODO: get/generate some valid byte data to verify the stamp() method
+ * TODO: verify <a href="http://www.ietf.org/rfc/rfc3161.txt">RFC 3161</a> compliance
  *
  * Status: in development
  *
  * Created by Petr Vsetecka on 3. 2. 2016.
  */
 public class TSAConnector {
+    private final boolean verbose = false;
 
     public static void main(String[] args) {
-        new TSAConnector().stamp();
+        new TSAConnector().stamp("data.txt");
     }
 
     /**
@@ -55,21 +62,36 @@ public class TSAConnector {
      * @throws IOException
      */
     public byte[] calculateMessageDigest(String input) throws NoSuchAlgorithmException, IOException {
-        SHA1Digest md = new SHA1Digest();
+        //SHA1Digest md = new SHA1Digest(); // returns same data as openssl command - good for testing
+        SHA256Digest md = new SHA256Digest();
 
         byte[] dataBytes = input.getBytes();
         int nread = dataBytes.length;
         md.update(dataBytes, 0, nread);
         byte[] result = new byte[32];
-        md.doFinal(result, 0);
+        int size = md.doFinal(result, 0);
 
-        return result;
+        return Arrays.copyOfRange(result, 0, size);
+    }
+
+    public byte[] readFile(String file) throws NoSuchAlgorithmException, IOException {
+        Path path = Paths.get(file);
+        String data = String.join(System.lineSeparator(), Files.readAllLines(path));
+
+        return calculateMessageDigest(data);
+    }
+
+    public byte[] readFileByte(String file) throws NoSuchAlgorithmException, IOException {
+        Path path = Paths.get(file);
+        byte[] data = Files.readAllBytes(path);
+
+        return data;
     }
 
     /**
      * method to send the TSA request and receive an answer
      */
-    private void stamp() {
+    private void stamp(String filename) {
         //System.setProperty("http.proxyHost", hostname);
         //System.setProperty("http.proxyPort", port);
 
@@ -80,20 +102,58 @@ public class TSAConnector {
         try {
 
             TimeStampRequestGenerator timeStampRequestGenerator = new TimeStampRequestGenerator();
-            //timeStampRequestGenerator.setReqPolicy(new ASN1ObjectIdentifier("1.3.6.1.4.1.13762.3"));
-            byte[] digest = calculateMessageDigest("helloo");
-            TimeStampRequest timeStampRequest = timeStampRequestGenerator.generate(TSPAlgorithms.SHA1, digest);
+            //timeStampRequestGenerator.setReqPolicy(new ASN1ObjectIdentifier("1.3.6.1.4.1.13762.3")); // not supported in CESNET
+            //byte[] digest = calculateMessageDigest("data");
+            byte[] digest = readFile(filename);
+            TimeStampRequest timeStampRequest = timeStampRequestGenerator.generate(TSPAlgorithms.SHA256, digest);
             //TimeStampRequest timeStampRequest = timeStampRequestGenerator.generate(TSPAlgorithms.SHA1, new byte[20], BigInteger.valueOf(100));
             byte request[] = timeStampRequest.getEncoded();
+            ASN1Object o = timeStampRequest.getMessageImprintAlgOID();
+
+
+            if (verbose) {
+                System.out.println("Request byte data:");
+                byte[] shouldBe = readFileByte("data.tsq");
+
+                System.out.println("target:");
+                for (byte b : shouldBe) {
+                    System.out.print((int) b + " ");
+                }
+                System.out.println();
+                System.out.println();
+
+                System.out.println("digest:");
+                for (byte b : digest) {
+                    System.out.print((int) b + " ");
+                }
+                System.out.println();
+                System.out.println();
+
+                System.out.println("generated:");
+                for (byte b : request) {
+                    System.out.print((int) b + " ");
+                }
+                System.out.println();
+                System.out.println();
+
+                /*
+                byte[] answer = readFileByte("data.tsr");
+                System.out.println("answer:");
+                for (byte b : answer) {
+                    System.out.print((char) b + "");
+                }
+                System.out.println();
+                System.out.println();//*/
+            }
 
             URL url = new URL(ocspUrl);
             con = (HttpURLConnection) url.openConnection();
 
             con.setDoOutput(true);
             con.setDoInput(true);
-            con.setRequestMethod("POST");
+            //con.setRequestMethod("POST");
             con.setRequestProperty("Content-type", "application/timestamp-query");
-            con.setRequestProperty("Content-length", String.valueOf(request.length));
+            //con.setRequestProperty("Content-length", String.valueOf(request.length));
 
             out = con.getOutputStream();
             out.write(request);
@@ -105,11 +165,29 @@ public class TSAConnector {
                 System.out.println("Response Code: ".concat(Integer.toString(con.getResponseCode())));
             }
             InputStream in = con.getInputStream();
+            /*
+            int i, size = 0;
+            char c;
+            while((i=in.read())!=-1)
+            {
+                // converts integer to character
+                c=(char)i;
+
+                // prints character
+                System.out.print(c);
+                size++;
+            }
+
+            System.out.println("\nResponse length: "+size);
+            if (size > 0)
+                return;//*/
             TimeStampResp resp = TimeStampResp.getInstance(new ASN1InputStream(in).readObject());
             TimeStampResponse response = new TimeStampResponse(resp);
             response.validate(timeStampRequest);
 
-            System.out.println("Status = ".concat(response.getStatusString()));
+            System.out.println("Status = ".concat((response.getStatusString() == null) ? "null (seems it means \"OK\")" :
+                    response.getStatusString()));
+
 
             if (response.getFailInfo() != null) {
 
@@ -152,20 +230,30 @@ public class TSAConnector {
                         System.out.println("the request cannot be handled due to system failure");
                         return;
                     }
+
+                    default: {
+                        System.out.println("Unknown (CESNET specific) error occurred!\n" +
+                                "Error code ("+response.getFailInfo().intValue()+") not specified in RFC 3161 standard.");
+                        return;
+                    }
                 }
             }
 
             System.out.print("Timestamp: ");
-            System.out.println(response.getTimeStampToken().getTimeStampInfo().getGenTime());
+            System.out.println(response.getTimeStampToken().getTimeStampInfo().getGenTime() == null ? "null" :
+                    response.getTimeStampToken().getTimeStampInfo().getGenTime());
 
             System.out.print("TSA: ");
-            System.out.println(response.getTimeStampToken().getTimeStampInfo().getTsa());
+            System.out.println(response.getTimeStampToken().getTimeStampInfo().getTsa() == null ? "null" :
+                    response.getTimeStampToken().getTimeStampInfo().getTsa());
 
             System.out.print("Serial number: ");
-            System.out.println(response.getTimeStampToken().getTimeStampInfo().getSerialNumber());
+            System.out.println(response.getTimeStampToken().getTimeStampInfo().getSerialNumber() == null ? "null" :
+                    response.getTimeStampToken().getTimeStampInfo().getSerialNumber());
 
             System.out.print("Policy: ");
-            System.out.println(response.getTimeStampToken().getTimeStampInfo().getPolicy());
+            System.out.println(response.getTimeStampToken().getTimeStampInfo().getPolicy() == null ? "null" :
+                    response.getTimeStampToken().getTimeStampInfo().getPolicy());
         } catch (Exception e) {
             e.printStackTrace();
         }
